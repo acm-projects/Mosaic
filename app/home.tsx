767396b04eab2dd8ddc0b54eacc97b4713e347a2
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-10-15 21:10:04 -0500

    basic homepage stuff

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { auth } from "@/lib/firebase_config";
+import { router } from "expo-router";
+import { signOut } from "firebase/auth";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function HomePage() {
     return (
-        <View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Home Page</Text>
+            <TouchableOpacity onPress={() => {
+                signOut(auth).then(() => {
+                    router.navigate('/login');
+                })
+            }} style={{marginTop: 20, padding: 10, backgroundColor: 'blue'}}> Sign out </TouchableOpacity>
         </View>
     );
 }
