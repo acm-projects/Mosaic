239204e6296b2188066083f7811b742f1ca5acd6
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-10-22 20:26:51 -0500

    added invite screen to layout

--- app/onboarding/groups/_layout.tsx
+++ app/onboarding/groups/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="join" options={{ headerShown: false }} />
             <Stack.Screen name="create" options={{ headerShown: false }} />
+            <Stack.Screen name="invite" options={{ headerShown: false }} />
         </Stack>
     );
 }
