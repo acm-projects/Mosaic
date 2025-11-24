2119ad2a09f507ed6875d7fb834b5a98afd41eff
Atharva Mishra <atharvamishra956+1@gmail.com>
2025-11-24 17:14:41 -0600

    added new error checking in index and new auth functions

--- app/index.tsx
+++ app/index.tsx
@@ -5,23 +5,24 @@ import { auth } from "@/lib/firebase_config";
 import { get_user_data } from "@/lib/firestore/users";
 import { router } from "expo-router";
 import { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { Alert, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
             if (user) {
                 const user = require_user();
-                const user_data = get_user_data(user!.uid);
+                const user_data = await get_user_data(user!.uid);
 
-                user_data.then((data) => {
-                    if (typeof (data) == "object" && !data?.taken_quiz) {
-                        router.navigate("/onboarding/quiz");
-                    } else {
-                        router.navigate("/home");
-                    }
-                });
+                if (!user_data.ok) {
+                    Alert.alert("Error", user_data.error);
+                } else if (user_data.data.taken_quiz) {
+                    router.navigate("/home");
+                } else {
+                    router.navigate("/onboarding/quiz");
+                }
+                
             } else {
                 router.navigate("/auth/login");
             }
@@ -35,6 +36,6 @@ export default function Index() {
             <PageBackground />
 
             <MosaicLogo size="lg" direction="column" show_subtitle={false} />
-        </SafeAreaView> 
+        </SafeAreaView>
     );
 };
